ee480c4479c58a4adfb4afa94ef6e9b92d01f076
Adilet <usenkanov.adilet.kanatbekovich@gmail.com>
2025-04-12 04:07:35 +0600

    before merge

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -31,7 +31,6 @@ function Home() {
             // window.location.reload();
         }
     }
-
     return (
         <Container className="home">
             <h3 className={"text-uppercase text-center"}>Slogan</h3>
@@ -40,24 +39,31 @@ function Home() {
                 Placeholder text is used to demonstrate the layout of the page and does not carry any meaning. It helps to see how the final design will look. Pay attention to fonts, line spacing, and margins to make the project harmonious.
             </div>
             <div className="division-line"></div>
-            <div className="browse-items">
-                <div className="browse-items__top">
-                    <div className="browse-items__left"><h2>Brows groups</h2></div>
-                    <div className="browse-items__right text-center">Explore all the components and pages below. They can
-                        be reused across all your projects and easily customized to fit your brand and project needs.
+              <div className="browse-items">
+                    <div className="browse-items__top">
+                          <div className="browse-items__left"><h2>Brows groups</h2></div>
+                          <div className="browse-items__right text-center">Explore all the components and pages below.
+                                They can
+                                be reused across all your projects and easily customized to fit your brand and project
+                                needs.
+                          </div>
                     </div>
-                </div>
-                <div className="division-line"></div>
-                <div className="browse-items__bottom">
-                    {
-                        groups?.map(post => (
-                            <HomeGroupCard key={post.id} id={post.id} title={post.title} description={post.description}>
-                            </HomeGroupCard>
-                        ))
-                    }
-                </div>
-                <Link to={"/groups/"}  className="browse-items__learn-more">Learn more</Link>
-            </div>
+                    <div className="division-line"></div>
+
+                    <iframe src="https://bakai.kg/ru/individual/credits/13/" width="900" height="300" frameBorder="0"
+                            scrolling="yes"></iframe>
+                    <div className="division-line"></div>
+                    <div className="browse-items__bottom">
+                          {
+                                groups?.map(post => (
+                                        <HomeGroupCard key={post.id} id={post.id} title={post.title}
+                                                       description={post.description}>
+                                        </HomeGroupCard>
+                                ))
+                          }
+                    </div>
+                    <Link to={"/groups/"} className="browse-items__learn-more">Learn more</Link>
+              </div>
         </Container>
     );
 }
